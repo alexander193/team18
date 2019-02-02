a0baa9605b89b9015f5852805edaa110b94fa6f7
alexander193 <cheesedn@gmail.com>
2019-02-02 17:29:46 +0700

    Add Unit Test in LearnedTest

---
+++ backend/src/test/java/sut/se/g18/LearnedTest.java
@@ -0,0 +1,177 @@
+package sut.se.g18;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.Before;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import sut.se.g18.Entity.LearnedEntity;
+import sut.se.g18.Repository.LearnedRepository;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class LearnedTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private LearnedRepository learnedRepository;
+
+    private Validator validator;
+
+    private SimpleDateFormat formatter5 = new SimpleDateFormat("E, MMM dd yyyy HH:mm:ss");
+
+
+
+    @Before
+    public void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void testSuccess() {
+        LearnedEntity L = new LearnedEntity();
+        L.setDetail("คอร์สเรียนตัดหญ้า");
+        L.setCheckObject(true);
+
+        try {
+            L.setDateLearned(formatter5.parse("Thu, Oct 18 2019 00:00:00"));
+            entityManager.persist(L);
+            entityManager.flush();
+
+            //fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 0);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TEST NULL DETAIL
+    @Test
+    public void testNullDetail() {
+        LearnedEntity L = new LearnedEntity();
+        L.setDetail(null);
+        L.setCheckObject(true);
+
+        try {
+            L.setDateLearned(formatter5.parse("Thu, Oct 18 2019 00:00:00"));
+            entityManager.persist(L);
+            entityManager.flush();
+
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TEST MIN SIZE DETAIL
+    @Test
+    public void testMinSizeDetail() {
+        LearnedEntity L = new LearnedEntity();
+        L.setDetail("คอร์");
+        L.setCheckObject(true);
+
+        try {
+            L.setDateLearned(formatter5.parse("Thu, Oct 18 2019 00:00:00"));
+            entityManager.persist(L);
+            entityManager.flush();
+
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 2); //ผิด Pattern ด้วย
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TEST MAX SIZE DETAIL
+    @Test
+    public void testMaxSizeDetail() {
+        LearnedEntity L = new LearnedEntity();
+        L.setDetail("คอร์สเรียนแต่งหน้า นั่งสมาธิ ดำน้ำ ปลูกปะการัง ทำอาหาร นวดสปา ปลูกป่า ดำนา ดูดิสนีย์ออนไอซ์ แรลลี่ตีกอล์ฟ ล่องเรือ ส่องสัตว์ ช๊อปปิ้ง ดูงิ้ว ดูละครเวที ดูคอนเสิร์ต " +
+                "ดินเนอร์ ทำขนม จัดดอกไม้ เที่ยวตลาดน้ำ เรียนถ่ายรูป ดูกายกรรม ชมเมืองเก่า เข้าสัมมนา ทัวร์ธรรมมะ เรียนเต้นแล้วก็ร้องเพลง");
+        L.setCheckObject(true);
+
+        try {
+            L.setDateLearned(formatter5.parse("Thu, Oct 18 2019 00:00:00"));
+            entityManager.persist(L);
+            entityManager.flush();
+
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TEST INVALID PATTERN DETAIL
+    @Test
+    public void testInvalidPatternDetail() {
+        LearnedEntity L = new LearnedEntity();
+        L.setDetail("คอร์เรียนแต่งหน้า นั่งสมาธิ ดำน้ำ ปลูกปะการัง");
+        L.setCheckObject(true);
+
+        try {
+            L.setDateLearned(formatter5.parse("Thu, Oct 18 2019 00:00:00"));
+            entityManager.persist(L);
+            entityManager.flush();
+
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TEST INVALID DATE
+    @Test
+    public void testInvalidDate() {
+        LearnedEntity L = new LearnedEntity();
+        L.setDetail("คอร์สเรียนตัดหญ้า");
+        L.setCheckObject(true);
+
+        try {
+            L.setDateLearned(formatter5.parse("Thu, Oct 18 2018 00:00:00"));
+            entityManager.persist(L);
+            entityManager.flush();
+
+            fail("Should not pass to this line");
+        } catch (javax.validation.ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(violations.isEmpty(), false);
+            assertEquals(violations.size(), 1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
